ede419fdb61380cb58e17478f143b7af49118a2e
A <awongpro7@gmail.com>
2021-03-11 03:09:54 +0000

    Adding file from Client setup

---
+++ play.js
@@ -0,0 +1,33 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const settings = { 
+  host: 'localhost',
+  port: 50541
+};
+
+const connect = function() {
+  console.log('Connecting ...');
+  const conn = net.createConnection(settings, () => {
+    console.log('connected to server!');
+    conn.write('This is an example');
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  // Event Handlers/Listeners
+  conn.on('data', (data) => {
+    console.log("data: ", data);
+    // Close connection if desired
+    // conn.end();
+  });
+
+  conn.on('end', () => {
+    // on closing, perform certain closing executions
+    console.log('disconnected from server');
+  });
+}
+
+connect();
