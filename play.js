76f25cd03cfcdfabe160ac4e8eb79e5af7f38bbb
A <awongpro7@gmail.com>
2021-03-11 03:16:22 +0000

    Adding files for Connection module assignment

--- play.js
+++ play.js
@@ -1,33 +1,4 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const settings = { 
-  host: 'localhost',
-  port: 50541
-};
-
-const connect = function() {
-  console.log('Connecting ...');
-  const conn = net.createConnection(settings, () => {
-    console.log('connected to server!');
-    conn.write('This is an example');
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  // Event Handlers/Listeners
-  conn.on('data', (data) => {
-    console.log("data: ", data);
-    // Close connection if desired
-    // conn.end();
-  });
-
-  conn.on('end', () => {
-    // on closing, perform certain closing executions
-    console.log('disconnected from server');
-  });
-}
-
+// play.js
+const { connect } = require('./client');
+console.log('Connecting ...');
 connect();
